c5c6bf5e8d91a167a162a3cb88ac14f473530977
george <george@Yvettes-MacBook-Pro.local>
2016-01-21 20:10:02 +1100

    4 commit

--- users/recipes/ishp.rb
+++ users/recipes/ishp.rb
@@ -1,11 +1,3 @@
-item = Chef::DataBagItem.new
-item.data_bag('userlist1')
-item['id'] = 'test_user1'
-item['password'] = '$1$5cE1rI/9$4p0fomh9U4kAI23qUlZVv/'
-item['uid'] = '9002'
-
-
-
 users_manage 'ishp' do
   group_id 3000
   action [:remove, :create]
