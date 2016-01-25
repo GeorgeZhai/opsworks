92d33249533693dfd03a9c152327e25d7f32588a
george <george@Yvettes-MacBook-Pro.local>
2016-01-26 10:40:10 +1100

    util

--- awslinuxutil/recipes/command.rb
+++ awslinuxutil/recipes/command.rb
@@ -3,3 +3,12 @@ execute "touch file" do
   command "touch /data/test-command-recipe.txt"
 end
 
+
+execute "adding date" do
+  command "date >> /data/test-command-recipe.txt"
+end
+
+execute "adding text" do
+  command "echo \'tested command execution......\'  >> /data/test-command-recipe.txt"
+end
+
